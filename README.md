b7cf890462b1089a7c36d44ef162d7b619072785
jmoralesss <mail@jmorales.net>
2024-02-22 13:40:43 -0600

    feat(init): initial commit to time repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# time
+
+A white-label self-hostable time management web application.
---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/fissilejonathan/time/view/page/home"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		name := "Jonathan Morales"
+
+		return home.Page(&name).Render(c.Request().Context(), c.Response().Writer)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
